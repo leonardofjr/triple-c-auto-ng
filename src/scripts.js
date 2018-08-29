1ce8426eb5dc1505029c815c970f32112b71f6e0
Leonardo Felipa <leonardo.f.jr@gmail.com>
2018-08-29 00:48:58 -0400

    updated script file

--- src/scripts.js
+++ src/scripts.js
@@ -1,15 +1,22 @@
-$(document).ready(() => {
+$('.toggle').click((e) => {
+    let target = $(e.currentTarget).data("target");
+    let collapsedStatus = $(e.currentTarget).data("collapsed");
 
-    $('#hamburger-button, #close-mobile-navigation-btn').click((e) => {
-        animateMobileNavigation(e, '#mobile-menu');
-    });
-
-    function animateMobileNavigation(e, target) {
-        $(target).animate({
-            opacity: 1,
-            width: "toggle"
-        }, 300, () => {
-        });
+    if (collapsedStatus !== 'on') {
+        $(target).addClass('slide-in');
+        $(e.currentTarget).addClass('slide-in-toggler');
+        $(e.currentTarget).data("collapsed", "on");
     }
+    else {
+        $(target).removeClass('slide-in');
+        $(e.currentTarget).removeClass('slide-in-toggler');
+        $(e.currentTarget).data("collapsed", "off");
+    }
+});
 
+
+$('#mobile-menu a').click((e) => {
+    $('#mobile-menu').removeClass('slide-in');
+    $('.mobile-nav-toggler').removeClass('slide-in-toggler');
+    $('.mobile-nav-toggler').data("collapsed", "off");
 })
