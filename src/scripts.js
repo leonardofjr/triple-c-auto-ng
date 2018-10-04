57bc291d8303e14b8de7ddcd1f0c8bc95052377a
Leo <leonardo.f.jr@gmail.com>
2018-10-04 17:17:12 -0400

    fixed

--- src/scripts.js
+++ src/scripts.js
@@ -1,45 +1,8 @@
 $(document).ready(function() {
-    $('.toggle').click((e) => {
-        let target = $(e.currentTarget).data("target");
-        let collapsedStatus = $(e.currentTarget).data("collapsed");
-
-        if (collapsedStatus !== 'on') {
-            $(target).addClass('slide-in');
-            $(e.currentTarget).addClass('slide-in-toggler');
-            $('body').addClass('slide-in-body');
-            $(e.currentTarget).data("collapsed", "on");
-        }
-        else {
-            $('body').removeClass('slide-in-body');
-            $(target).removeClass('slide-in');
-            $(e.currentTarget).removeClass('slide-in-toggler');
-            $(e.currentTarget).data("collapsed", "off");
-        }
-    });
-
-
-    $('#mobile-menu a').click((e) => {
-        $('body').removeClass('slide-in-body');
-        $('#mobile-menu').removeClass('slide-in');
-        $('.mobile-nav-toggler').removeClass('slide-in-toggler');
-        $('.mobile-nav-toggler').data("collapsed", "off");
+    /* This script is to collapse the bootstrap mobile menu when a nav item is clicked */
+    /* When a nav item is clicked */
+    $('.dropdown-item , .nav-item').click(function(e) {
+        /* we will remove the bootstrap show class */
+        $('#navbarNavDropdown').removeClass('show');
     })
-
-
-    $('.toggle-free-quote-toggler').click((e) => {
-        let target = $(e.currentTarget).data("target");
-        let collapsedStatus = $(e.currentTarget).data("collapsed");
-
-        if (collapsedStatus !== 'on') {
-            $(target).addClass('slide-in-fixed-free-quote-container');
-            $(e.currentTarget).addClass('slide-in-fixed-free-quote-form-toggler');
-            $(e.currentTarget).data("collapsed", "on");
-        }
-        else {
-            $(target).removeClass('slide-in-fixed-free-quote-container');
-            $(e.currentTarget).removeClass('slide-in-fixed-free-quote-form-toggler');
-            $(e.currentTarget).data("collapsed", "off");
-        }
-    });
-   
 })
